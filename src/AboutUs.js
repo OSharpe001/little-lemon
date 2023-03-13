6642ca7ce7ba63d54558f5d2af15fdfff932fc74
O. Sharpe <osharpe001@gmail.com>
2023-03-13 01:22:53 -0400

    added background image to AboutUs page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -10,12 +10,27 @@ export default function AboutUs() {
                             Little Lemon is owned by two Italian brothers, Mario and
                             Adrian, who moved to the United States to pursue their
                             shared dream of owning a restaurant.
+                        </p>
+                        <p>
+                            To craft the menu, Mario relies on family recipes and his
+                            experience as a chef in Italy. Adrian does all the
+                            marketing for the restaurant and led the effort to expand
+                            the menu beyond classic Italian to incorporate additional
+                            cuisines from the mediterranean region.
+                        </p>
+                        <p>
                             The restaurant has a rustic and relaxed atmosphere with
-                            moderate prices, making it a popular place for a meal any
-                            time of the day.
-                            <br/>
+                            moderate prices, making it a popular place for lunch or
+                            dinner, especially with friends.
+                        </p>
+                        <p>
+                            Based in New York City, New York, the chefs draw inspiration
+                            from Italian, Greek, and Turkish cultures and have a menu
+                            of 12-15 items that they rotate seasonally.
+                        </p>
+                        <p>
                             We've updated our menu with new dinners!
-                            Come in and enjoy our "home-cooked" flavors. Your meal
+                            Come in and enjoy our Mediteranean flavors. Your meal
                             will be 20% off (for a limited time)!
                         </p>
                 </article>
