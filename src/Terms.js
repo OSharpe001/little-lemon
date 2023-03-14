c5a56c9f97db169b1e7ad1f5953e7bc01885605b
O. Sharpe <osharpe001@gmail.com>
2023-03-14 18:42:19 -0400

    changed the reservation Terms page layout

--- src/Terms.js
+++ src/Terms.js
@@ -4,6 +4,13 @@ export default function Terms() {
   return (
     <section className="reservation-terms">
       <h1>Our Reservation Terms of Service</h1>
+      <p>Little Lemon is NOT an actual restaurant and
+        as such, have no right to hold you to actual
+        terms since we have no actual service.
+        <br/>
+        We do hope you enjoy navigating our website,
+        though.
+      </p>
     </section>
   );
 };
