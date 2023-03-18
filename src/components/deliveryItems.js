7259771ec2d70c48f18adb5695a42e70a47c98c3
O. Sharpe <osharpe001@gmail.com>
2023-03-18 11:11:08 -0400

    started making a directory (deliveryItems)

---
+++ src/components/deliveryItems.js
@@ -0,0 +1,61 @@
+export const menu = [
+    {
+        name:"Bruchetta",
+        image:"bruchetta.png",
+        alt:"sliced and toasted italian bread topped with mozzarella, tomatoes, basil and olive oil.",
+        description:"sliced and toasted italian bread topped with mozzarella, tomatoes, basil and olive oil.",
+        price:"$7.00"
+    },
+
+
+    {
+        name:"Duck Spring Roll",
+        description:"2 pieces.",
+        price:"$17.00"
+    },
+    {
+        name:"Hummus Platters",
+        description:"Mushrooms.",
+        price:"$15.00"
+    },
+    {
+        name:"Half Roasted Eggplant with Chick Peas Parsley, Tahini,  and Lemon",
+        description:"",
+        price:"$13.00"
+    },
+    {
+        name:"Bistro BBQ Brisket Bites",
+        description:"",
+        price:"$19.00"
+    },
+    {
+        name:"Short Rib Tacos 3pcs",
+        description:"",
+        price:"$23.00"
+    },
+    {
+        name:"Onion Rings",
+        description:"Vidalia onions coated in seasoned breadcrumbs.",
+        price:"$10.00"
+    },
+    {
+        name:"Mexican Wantons (3 Pcs.)",
+        description:"Poached chicken with dried chili, garlic, cilantro, jalapeño Served with ancho chili remoulade.",
+        price:"$25.00"
+    },
+    {
+        name:"Moroccan Cigars (5 pcs)",
+        description:"Ground beef, and lamb with chili pepper and cilantro. Served with tahini.",
+        price:"$19.00"
+    },
+    {
+        name:"Dim Sum",
+        description:"Steamed Dim Sum, ground chicken, ginger, scallion, and soy.",
+        price:"$17.00"
+    },
+    {
+        name:"Zucchini Chips",
+        description:"Served with spicy tomato and crispy lettuce.",
+        price:"$16.00"
+    }
+]
