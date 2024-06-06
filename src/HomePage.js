c74f00194b62180ea04537769d1002de622e719b
O. Sharpe <osharpe001@gmail.com>
2024-06-06 10:44:19 -0400

    live test2 of home page to fix grid

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,11 +7,11 @@ import { Link } from "react-router-dom";
 export default function Homepage() {
     return (
         <>
-            {/* <Hero /> */}
-            <section className="declare1">
+            <Hero />
+            {/* <section className="declare1">
                 <h2>This week's specials!</h2>
                 <Link aria-label="On Click" to="/order" className="button">Online Menu</Link>
-            </section>
+            </section> */}
             <br/>
             <Specials />
             <h2 className="declare2">Testimonials</h2>
