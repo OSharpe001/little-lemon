69c35faf8690b734b4fcbdf09f0bd831f2d53cdb
O. Sharpe <osharpe001@gmail.com>
2024-06-06 12:28:43 -0400

    live test5. took specials off of the css

--- src/HomePage.js
+++ src/HomePage.js
@@ -12,7 +12,7 @@ export default function Homepage() {
                 <h2>This week's specials!</h2>
                 <Link aria-label="On Click" to="/order" className="button">Online Menu</Link>
             </section>
-            {/* <br/> */}
+            <br/>
             <Specials />
             <h2 className="declare2">Testimonials</h2>
             <Testimonials />
