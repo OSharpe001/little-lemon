18eb5b4575857c3fe10be7d39a4d69dbc195a802
O. Sharpe <osharpe001@gmail.com>
2024-06-06 12:32:17 -0400

    live test6. consolidate Specials component into home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,10 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
-import Specials from "./components/Specials";
+// import Specials from "./components/Specials";
+import greek_salad from "../assets/images/greek_salad.jpg";
+import bruchetta from "../assets/images/bruchetta.png";
+import lemon_dessert from "../assets/images/lemon_dessert.jpg";
+import moped_icon from "../assets/images/moped_icon_option.png";
 import Testimonials from "./components/Testimonials";
 import { Link } from "react-router-dom";
 
@@ -13,7 +17,72 @@ export default function Homepage() {
                 <Link aria-label="On Click" to="/order" className="button">Online Menu</Link>
             </section>
             <br/>
-            <Specials />
+            {/* <Specials /> */}
+            <section className="specials">
+            <article>
+                <img
+                    className="specials-img"
+                    src={greek_salad}
+                    alt="salad"
+                    />
+                <div className="name-price">
+                    <h4>Greek Salad</h4>
+                    <p className="price">$8.00</p>
+                </div>
+                <div className="specials-text">
+                    <p className="specials-text-p">The famous greek salad of crispy lettuce, peppers, olives and our New York style feta cheese garnished with crunchy garlic and rosemary croutons.</p>
+                    <a aria-label="On Click" className="specials-a" href="/order" >Order a delivery
+                        <img
+                            className="moped"
+                            src={moped_icon}
+                            alt="moped"
+                        />
+                    </a>
+                </div>
+            </article>
+            <article>
+                <img
+                    className="specials-img"
+                    src={bruchetta}
+                    alt="platter of toasted bread topped with tomatoes and seasoning"
+                    />
+                <div className="name-price">
+                    <h4>Bruchetta</h4>
+                    <p className="price">$5.00</p>
+                </div>
+                <div className="specials-text">
+                    <p className="specials-text-p">Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.</p>
+                    <a aria-label="On Click" className="specials-a" href="/order">Order a delivery
+                    <img
+                        className="moped"
+                        src={moped_icon}
+                        alt="moped"
+                        />
+                    </a>
+                </div>
+            </article>
+            <article>
+                <img
+                    className="specials-img"
+                    src={lemon_dessert}
+                    alt="slice of lemon cake with lemons in the background"
+                    />
+                <div className="name-price">
+                    <h4>Lemon Dessert</h4>
+                    <p className="price">$7.00</p>
+                </div>
+                <div className="specials-text">
+                    <p className="specials-text-p">This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.</p>
+                    <a aria-label="On Click" className="specials-a" href="/order">Order a delivery
+                        <img
+                            className="moped"
+                            src={moped_icon}
+                            alt="moped"
+                        />
+                    </a>
+                </div>
+            </article>
+        </section>
             <h2 className="declare2">Testimonials</h2>
             <Testimonials />
             <br/>
